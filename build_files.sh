8ccbd707aadd81a0dc6dfe120a33c6a81bc516d2
Satyam <justsaysatyam@gmail.com>
2026-04-28 16:03:59 +0530

    error

--- build_files.sh
+++ build_files.sh
@@ -8,5 +8,9 @@ python3.12 -m pip install -r requirements.txt
 # Collect static files
 python3.12 manage.py collectstatic --noinput --clear
 
+# Run migrations
+python3.12 manage.py migrate --noinput
+
+
 
 echo "BUILD END"
