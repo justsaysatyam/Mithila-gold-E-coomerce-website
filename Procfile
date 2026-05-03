e79f7381e1f5408e7cf9ef5438c465b59ffe95a5
Satyam <justsaysatyam@gmail.com>
2026-05-03 17:09:04 +0530

    memory

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python manage.py migrate && gunicorn poultry_farm.wsgi
+web: python manage.py migrate && gunicorn poultry_farm.wsgi --worker-class gevent --workers 4 --timeout 120 --bind 0.0.0.0:10000
--- poultry_farm/urls.py
+++ poultry_farm/urls.py
@@ -3,7 +3,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.generic import RedirectView
+
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url='/static/img/logo.png')), # Or your specific favicon path
     path('', include('store.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
